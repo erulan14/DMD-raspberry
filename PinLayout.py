65670fb68ccdbe30f13c073ab48b6933dbba56fe
erulan14 <42293127+erulan14@users.noreply.github.com>
2021-08-20 14:58:50 +0600

    Update PinLayout.py

--- PinLayout.py
+++ PinLayout.py
@@ -2,12 +2,10 @@ class PinLayout(object):
     OE = 24
     A = 39
     B = 40
-    CLK = 23  # CLOCK [CLK]
     STROBE = 37  # [SLCK] [LATCH] [STROBE]
 
-    def __init__(self, a, b, clk, latch, oe):
+    def __init__(self, a, b, latch, oe):
         self.A = a
         self.B = b
-        self.CLK = clk
         self.STROBE = latch
         self.OE = oe
